1984957581ec0f28f521f85342d7e1e12259cdca
grasilv1599 <grasisilva159@gmail.com>
2025-05-23 10:12:59 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/adhoc-teste/" : "/",
   server: {
     host: "::",
     port: 8080,
